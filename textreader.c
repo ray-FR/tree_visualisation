2391c542b5679ffbf9bcead0498ed13144127fd9
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-11-10 12:15:15 +0100

    some changes to the addnewword function

--- textreader.c
+++ textreader.c
@@ -5,6 +5,28 @@
 #include <ctype.h>
 #include "textreader.h"
 
+int issep(char c) {
+    unsigned char uc = (unsigned char)c;
+    return isspace(uc) || c == '!' || c == '?' || c == '.' ||
+           c == '-' || c == ',' || c == ';' || c == ':';
+}
+void chevec_push_back(chvec_t* vec, char* data){
+    if(vec->size == vec->max_size) {
+        void * ptr;
+        vec->max_size = vec->max_size * 2;
+        ptr = realloc(vec->data, vec->max_size * sizeof **(vec->data));
+        if(ptr == NULL) {
+        free(vec->data);
+        free(vec);
+        assert(ptr);
+        } else
+        vec->data = ptr;
+    }
+    vec->data[vec->size] = data;
+    ++(vec->size);
+}
+
+
 chvec_t * newvec(void) {
   chvec_t * vec = malloc(1 * sizeof *vec);
   assert(vec);
@@ -15,13 +37,42 @@ chvec_t * newvec(void) {
   return vec;
 }
 
-void addnewword(chvec_t* vec){}
+void addnewword(chvec_t* vec, char* buff){
+    int len;
+    int i = 0;
+    char* mot;
+    int isinmot = 0;
+    int start;
+    while(buff[i] != '\n'){
+        if (!issep(buff[i])){
+            //printf("%c", buff[i]);
+            if (!isinmot){
+            start = i;
+            isinmot = 1;}
+        }
+        else {
+            if (isinmot){
+                len = i - start;
+                
+                mot = malloc((len+1) * sizeof *mot);
+                assert(mot);
+                strncpy(mot, &buff[start], len);
+                mot[len] = '\0';
+                //printf("%d: %s\n", len, mot);
+                chevec_push_back(vec, mot);
+                // free(mot);
+                isinmot = 0;
+                vec->size++;
+                
+            }
 
-int issep(char txt){
-    if (isspace(txt) || txt == '!' || txt == '?' || txt == '.' || txt == '-' || txt == ',' || txt == ';' || txt == ':'){
-        return 1;
+        }
+        i++;
     }
-    return 0;}
+    return;
+}
+
+
 
 
 
@@ -33,28 +84,12 @@ int main(){
     assert(te);
     
     while (fgets(buff, sizeof buff, stdin) != NULL){
-        int i = 0;
-        int taillem = 1;
-        int iss = 0;
-        while(buff[i] != '\n'){
-            if (!issep(buff[i])){
-                iss = 0;
-                taillem++;
-            }
-            else{
-                if (!iss){
-                    // te = movetonewvec(numvec, te, taillem);
-                    // strncpy(te[numvec].mot, &(buff[i]), taillem);
-                    taillem = 0;
-                    iss = 1;
-                }
-            }
-            i++;
-        }    
-        // for (int i = 0; i<numvec;i++){
-        //     printf("%d: %s", te[i].t, te[i].mot);
-        // }
-    
+        addnewword(te, buff);
+        
+        for (int i = 0; i<te->size;i++){
+            printf("%d: %s\n", i, te->data[i]);
+        }
+        printf("\n\n");
 
     }
 
