b860b3af15c8d3f35ee2d2c05bedbc1e62affa41
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-11-13 22:05:57 +0100

    Write the main.c content to init a g.dot file

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "graph.h"
+
+int main(int argc, char** argv){
+    chvec_t* te;
+    FILE* f;
+    char buff[1024];
+    te = newvec();
+    assert(te);
+    
+    while (fgets(buff, sizeof buff, stdin) != NULL){
+        addnewword(te, buff);
+        
+        
+    }
+    f = fopen("stuff/g.dot", "w");
+    prepareFile(f, te);
+    fclose(f);
+    for(int i = 0; i<te->size; i++){
+        free(te->data[i]);
+    }
+    free(te->data);
+    return 0;
+
+}
