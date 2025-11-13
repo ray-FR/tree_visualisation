447ac20524dd480c1b1fa81c337b612caf810909
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-11-13 22:04:53 +0100

    implement first ish version of addgraph + some small tweaks to prepareFile

--- graph.c
+++ graph.c
@@ -1,21 +1,31 @@
 #include "graph.h"
 
 void prepareFile(FILE* f, chvec_t* v){
-    fprintf(f, "diagraph G {\n"); 
-    addGraph(f, v, v->size, v->size);
-    fprintf(f, "}\0");
+    fprintf(f, "digraph G {\n"); 
+    addGraph(f, v, v->size/2, v->size);
+    fprintf(f, "}\n");
+    
     
 
 }
 
 void addGraph(FILE* f, chvec_t* v, int ind, int nbrelem){
-    
     int ind_mb;
     int ind_mt;
-    while (nbrelem >= 1){
-        if (nbrelem == 1) fprintf(f, "%s\n", v->data[ind]); return;
-        
+    
+    
+    if (nbrelem == 1) {
+        return;
+    } 
+    ind_mb = ind / 2;
+    if (ind_mb != ind) fprintf(f, "%s -> %s\n", v->data[ind], v->data[ind_mb]);
 
-    }
+    ind_mt = ind + (nbrelem - ind) / 2;
+    if (ind_mt != ind) fprintf(f, "%s -> %s\n", v->data[ind], v->data[ind_mt]);
+
+    if (ind_mb != ind) addGraph(f, v, ind_mb, nbrelem / 2);
+    if (ind_mt != ind) addGraph(f, v, ind_mt, nbrelem - ind_mb);
+
+    
     
 }
