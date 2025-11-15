ffc212894a57ffb68ff7a629caa2b755130d7a74
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-11-15 13:01:23 +0100

    did some modifs to make it work with struct.h and remove unnecessary includes

--- include/graph.h
+++ include/graph.h
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
-#include <ctype.h>
-#include "textreader.h"
+#include "struct.h"
 
 
 extern void prepareFile(FILE* f, chvec_t* v);
--- include/textreader.h
+++ include/textreader.h
@@ -1,22 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <string.h>
-#include <ctype.h>
-
+#include "struct.h"
 
 
 
   
-typedef struct chvec {
-    /*! donne le nombre d'élements stockés dans le chvec. */
-    size_t size;
-    /*! max_size est le nombre (actuellement) maximal d'éléments à stocker */
-    size_t max_size;
-    /*! data est le pointeur vers le tableau dynamique d'éléments (ici des int) à stocker. */
-    char ** data;
 
-} chvec_t;
 
 extern int issep(char txt);
 
