78c2115d7cf481db4e433ec9fe5fb0e649ddc56c
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-11-15 16:48:24 +0100

    implement basic tree functions

--- include/arb.h
+++ include/arb.h
@@ -2,6 +2,7 @@
 #define ARB_H
 #include "struct.h"
 
-
-
+extern arb_t* allC(char* v, arb_t* fg, arb_t* fd);
+extern void priC(arb_t* a);
+extern void libC(arb_t** a);
 #endif
--- src/arb.c
+++ src/arb.c
@@ -1,1 +1,30 @@
 #include "../include/arb.h"
+
+
+arb_t* allC(char* v, arb_t* fg, arb_t* fd){
+    arb_t* r = malloc (sizeof *r);
+    assert(r);
+    r->name = v;
+    r->g = fg;
+    r->d = fd;
+    return r;
+}
+
+void priC(arb_t* a){
+    // pour afficher un arbre (par un parcours préfixe)
+    if(a==NULL){
+        printf("V "); // V pour vide
+        return;
+    }
+    printf("%s ",a->name);
+    priC(a->g);
+    priC(a->d);
+}
+
+void libC(arb_t** a){
+    if(*a==NULL) return;
+    libC(&(**a).g);
+    libC(&(**a).d);
+    free(*a);
+    *a=NULL;
+}
