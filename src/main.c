4c0955a04ac28f6a253d99c319df1c31e2e374c6
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-11-22 10:09:53 +0100

    Modified main to use an argument directly input by the user as to where to put the g.dot file

--- src/main.c
+++ src/main.c
@@ -7,6 +7,10 @@
 
 
 int main(int argc, char** argv){
+    if (argc < 2) {
+        printf("Usage: cat path/to/file | ./graph where/to/save+name.dot\n");
+        exit(1);
+    }
     chvec_t* te;
     FILE* f;
     arb_t* a;
@@ -16,13 +20,12 @@ int main(int argc, char** argv){
     
     while (fgets(buff, sizeof buff, stdin) != NULL) addnewword(te, buff);
     
-    for (int i = 0; i<te->size; i++) printf("%d %s\n", i, te->data[i]);
     
     assC(&a, te, 0, te->size - 1);
-    printf("\n");
-    f = fopen("stuff/g.dot", "w");
+    f = fopen(argv[1], "w");
     prepareFile(f, te, a);
     fclose(f);
+    printf("Done! saved to %s\n", argv[1]);
     for(int i = 0; i<te->size; i++){
         free(te->data[i]);
     }
