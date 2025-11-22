64e59db8aab628609c5bace29ac6ff212aaeace4
Ray <rayan.izem02@etud.univ-paris8.fr>
2025-11-22 09:58:41 +0100

    Change allC args to take into account the new available allC

--- include/arb.h
+++ include/arb.h
@@ -2,7 +2,7 @@
 #define ARB_H
 #include "struct.h"
 
-extern arb_t* allC(char* v, arb_t* fg, arb_t* fd);
+extern arb_t* allC(char* v, int ind, arb_t* fg, arb_t* fd);
 extern void priC(arb_t* a);
 extern void libC(arb_t** a);
 extern void assC(arb_t** a, chvec_t* v, int start, int end);
